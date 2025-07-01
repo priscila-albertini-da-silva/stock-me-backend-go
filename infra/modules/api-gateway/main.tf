94a021014ecda59246ad76dc76551835557a1520
Priscila Albertini da Silva <priscila.albertini92@gmail.com>
2025-07-01 10:24:30 -0300

    Criando api gateway resource

--- infra/modules/api-gateway/main.tf
+++ infra/modules/api-gateway/main.tf
@@ -10,79 +10,133 @@ resource "aws_api_gateway_rest_api_policy" "api_gateway_policy" {
 }
 
 # [create_storage_location] API: METHOD, INTEGRATION
+resource "aws_api_gateway_resource" "api_gateway_resource_create_storage_location" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = var.path_part
+
+  depends_on = [aws_api_gateway_rest_api.api_gateway]
+}
+
 resource "aws_api_gateway_method" "api_gateway_method_create_storage_location" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource_create_storage_location.id
   http_method   = var.create_storage_location_http_method
   authorization = "NONE"
-  depends_on    = [aws_api_gateway_rest_api.api_gateway]
+  depends_on    = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_resource.api_gateway_resource_create_storage_location]
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_create_storage_location" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource_create_storage_location.id
   http_method             = var.create_storage_location_http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
-  depends_on              = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_method.api_gateway_method_create_storage_location]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_method.api_gateway_method_create_storage_location,
+    aws_api_gateway_resource.api_gateway_resource_create_storage_location
+  ]
 }
 
 # [update_storage_location] API: METHOD, INTEGRATION
+resource "aws_api_gateway_resource" "api_gateway_resource_update_storage_location" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_resource.api_gateway_resource_create_storage_location.id
+  path_part   = var.path_part
+
+  depends_on = [aws_api_gateway_rest_api.api_gateway]
+}
+
 resource "aws_api_gateway_method" "api_gateway_method_update_storage_location" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource_update_storage_location.id
   http_method   = var.update_storage_location_http_method
   authorization = "NONE"
-  depends_on    = [aws_api_gateway_rest_api.api_gateway]
+  depends_on    = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_resource.api_gateway_resource_update_storage_location]
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_update_storage_location" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource_update_storage_location.id
   http_method             = var.update_storage_location_http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
-  depends_on              = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_method.api_gateway_method_update_storage_location]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_method.api_gateway_method_update_storage_location,
+    aws_api_gateway_resource.api_gateway_resource_update_storage_location
+  ]
 }
 
 # [find_storage_location] API: METHOD, INTEGRATION
+resource "aws_api_gateway_resource" "api_gateway_resource_find_storage_location" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = var.path_part
+
+  depends_on = [aws_api_gateway_rest_api.api_gateway]
+}
+
 resource "aws_api_gateway_method" "api_gateway_method_find_storage_location" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource_find_storage_location.id
   http_method   = var.find_storage_location_http_method
   authorization = "NONE"
-  depends_on    = [aws_api_gateway_rest_api.api_gateway]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_resource.api_gateway_resource_find_storage_location
+  ]
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_find_storage_location" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource_find_storage_location.id
   http_method             = var.find_storage_location_http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
-  depends_on              = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_method.api_gateway_method_find_storage_location]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_method.api_gateway_method_find_storage_location,
+    aws_api_gateway_resource.api_gateway_resource_find_storage_location
+  ]
 }
 
 # [remove_storage_location] API: METHOD, INTEGRATION
+resource "aws_api_gateway_resource" "api_gateway_resource_remove_storage_location" {
+  rest_api_id = aws_api_gateway_rest_api.api_gateway.id
+  parent_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  path_part   = var.path_part
+
+  depends_on = [aws_api_gateway_rest_api.api_gateway]
+}
+
 resource "aws_api_gateway_method" "api_gateway_method_remove_storage_location" {
   rest_api_id   = aws_api_gateway_rest_api.api_gateway.id
-  resource_id   = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id   = aws_api_gateway_resource.api_gateway_resource_remove_storage_location.id
   http_method   = var.remove_storage_location_http_method
   authorization = "NONE"
-  depends_on    = [aws_api_gateway_rest_api.api_gateway]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_resource.api_gateway_resource_remove_storage_location
+  ]
 }
 
 resource "aws_api_gateway_integration" "lambda_integration_remove_storage_location" {
   rest_api_id             = aws_api_gateway_rest_api.api_gateway.id
-  resource_id             = aws_api_gateway_rest_api.api_gateway.root_resource_id
+  resource_id             = aws_api_gateway_resource.api_gateway_resource_remove_storage_location.id
   http_method             = var.remove_storage_location_http_method
   integration_http_method = "POST"
   type                    = "AWS_PROXY"
   uri                     = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${var.lambda_arn}/invocations"
-  depends_on              = [aws_api_gateway_rest_api.api_gateway, aws_api_gateway_method.api_gateway_method_remove_storage_location]
+  depends_on = [
+    aws_api_gateway_rest_api.api_gateway,
+    aws_api_gateway_method.api_gateway_method_remove_storage_location,
+    aws_api_gateway_resource.api_gateway_resource_remove_storage_location
+  ]
 }
 
 # DEPLOYMENT, STAGE and METHOD SETTINGS
